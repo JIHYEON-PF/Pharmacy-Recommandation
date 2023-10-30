44d073b4f0783060ff035aeb2a1cdbdf59aab039
JIHYEON <jhkim@aptner.com>
2023-10-30 23:51:18 +0900

    chore:init project

---
+++ src/main/java/com/openapi/pharmacyrecommandation/PharmacyRecommandationApplication.java
@@ -0,0 +1,13 @@
+package com.openapi.pharmacyrecommandation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommandationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyRecommandationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openapi/pharmacyrecommandation/PharmacyRecommandationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.pharmacyrecommandation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommandationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
