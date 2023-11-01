db7f689dfb24e31296fcc8ddf5cd71a900e55d8e
JIHYEON <jhkim@aptner.com>
2023-11-02 01:04:29 +0900

    feature: #4 - create entity and repository
    - create pharmacy domain and repository

---
+++ src/main/java/com/openapi/pharmacyrecommandation/pharmacy/entity/Pharmacy.java
@@ -0,0 +1,27 @@
+package com.openapi.pharmacyrecommandation.pharmacy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "pharmacy")
+public class Pharmacy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String pharmacyName;
+    private String pharmacyAddress;
+    private double latitude;
+    private double longitude;
+}
