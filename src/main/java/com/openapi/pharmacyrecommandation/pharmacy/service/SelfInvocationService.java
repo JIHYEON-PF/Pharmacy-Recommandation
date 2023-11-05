dca9022bb10a0baa14fd401dea53aadd74c8fa3d
JIHYEON <jhkim@aptner.com>
2023-11-06 00:39:17 +0900

    feature:#7 self-invocation
    - annotated @Transaction but rollback is not work because of self invocation

---
+++ src/main/java/com/openapi/pharmacyrecommandation/pharmacy/service/SelfInvocationService.java
@@ -0,0 +1,33 @@
+package com.openapi.pharmacyrecommandation.pharmacy.service;
+
+import com.openapi.pharmacyrecommandation.pharmacy.entity.Pharmacy;
+import com.openapi.pharmacyrecommandation.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SelfInvocationService {
+    private final PharmacyRepository pharmacyRepository;
+
+    public void bar(List<Pharmacy> pharmacies) {
+        log.info("bar CurrentTransactionName: {}", TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacies);
+    }
+
+    @Transactional
+    public void foo(List<Pharmacy> pharmacies) {
+        log.info("foo CurrentTransactionName: {}", TransactionSynchronizationManager.getCurrentTransactionName());
+
+        pharmacies.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error");
+        });
+    }
+}
---
+++ src/test/groovy/com/openapi/pharmacyrecommandation/pharmacy/service/SelfInvocationServiceTest.groovy
@@ -0,0 +1,41 @@
+package com.openapi.pharmacyrecommandation.pharmacy.service
+
+import com.openapi.pharmacyrecommandation.AbstractIntegrationContainerBaseTest
+import com.openapi.pharmacyrecommandation.pharmacy.entity.Pharmacy
+import com.openapi.pharmacyrecommandation.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class SelfInvocationServiceTest extends AbstractIntegrationContainerBaseTest {
+    @Autowired
+    private SelfInvocationService selfInvocationService;
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    def setup() {
+        pharmacyRepository.deleteAll();
+    }
+
+    def "self invocation"() {
+        given:
+        String address = "서울 특별시 성북구 종암동";
+        String name = "은혜약국";
+        double latitude = 36.11;
+        double longitude = 128.11;
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+
+        when:
+        selfInvocationService.bar(Arrays.asList(pharmacy));
+
+        then:
+        def exception = thrown(RuntimeException.class);
+        def result = pharmacyRepository.findAll();
+        result.size() == 1;
+    }
+}
